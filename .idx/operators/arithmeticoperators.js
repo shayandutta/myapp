70d2e186f170ce06c7a33e436489df00ce166e5a
shayandutta23 <shayandutta23@gmail.com>
2024-07-12 12:42:21 +0000

    operators added

---
+++ .idx/operators/arithmeticoperators.js
@@ -0,0 +1,9 @@
+let x = 3;
+let y =2;
+
+console.log(x-y);
+console.log(x+y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+console.log(x**y);
---
+++ .idx/operators/assignmentoperators.js
@@ -0,0 +1,15 @@
+let x = 5;
+x +=5;
+console.log(x);
+
+x-+5;
+console.log(x);
+
+x*=5;
+console.log(x);
+
+x/=5;
+console.log(x);
+
+x%=5;
+console.log(x);
