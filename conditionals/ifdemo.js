afef5623830f7c716276a45b4a5e9eb6522502f9
shayandutta23 <shayandutta23@gmail.com>
2024-07-18 17:19:01 +0000

    added conditionals

---
+++ conditionals/ifdemo.js
@@ -0,0 +1,12 @@
+console.log("starting...");
+let isUserSignedIn = true;
+let isPaidUser = true;
+
+if (isUserSignedIn == true && isPaidUser == true) {
+    //this particular block will only execute if the condition mentioned is true
+    console.log("show the dashboard");
+}
+else{
+    console.log("Show the homepage");
+}
+console.log("End...")
