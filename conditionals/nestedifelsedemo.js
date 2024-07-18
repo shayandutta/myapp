875ef1a69a0b68ce8db6dcc685f1b735fd9c7153
shayandutta23 <shayandutta23@gmail.com>
2024-07-18 17:37:58 +0000

    added nested if else

---
+++ conditionals/nestedifelsedemo.js
@@ -0,0 +1,15 @@
+let isUserSubscribed = true;
+let userHasLionsGatePlay = true;
+let userHasSony = true;
+
+if(isUserSubscribed){
+    if(userHasLionsGatePlay){
+        console.log("Watch john wick");
+    }
+    else if(userHasSony){
+        console.log("Show the guardian");
+    }
+    else{
+        console.log("show the panchayat");
+    }
+}
