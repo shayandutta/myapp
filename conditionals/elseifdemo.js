b6857fc1ab13088e1f59bc0f2d9f6591f6115183
shayandutta23 <shayandutta23@gmail.com>
2024-07-18 17:28:06 +0000

    added new conditional elseif

---
+++ conditionals/elseifdemo.js
@@ -0,0 +1,13 @@
+let userSubscription = "premium";
+
+if(userSubscription == "unsubscribed"){
+    console.log("show pricing page");
+}
+
+else if(userSubscription == "super"){
+    console.log("show only hindi content");
+}
+else{
+    console.log("show all content");
+}
+console.log("end");
